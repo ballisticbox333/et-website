c2671f8aa022e81986a6dff1fc55ba42fdd0091b
Your Name <you@example.com>
2026-01-23 19:01:18 -0500

    Add Instagram social link and fix contact page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,9 +61,9 @@ function Home() {
 
       {/* Facebook section */}
       <div className="mt-14">
-        <h2 className="text-center">Check out our projects on Facebook!</h2>
+        <h2 className="text-center">Check out our projects on social media!</h2>
 
-        <div className="flex justify-center mt-6">
+        <div className="flex justify-center gap-6 mt-6">
           <a
             href="https://www.facebook.com/etcustomlandscaping"
             target="_blank"
@@ -77,6 +77,21 @@ function Home() {
               className="w-6 h-6 fill-white"
             >
               <path d="M279.14 288l14.22-92.66h-88.91V117.78c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/>
+            </svg>
+          </a>
+          <a
+            href="https://www.instagram.com/etcustomlandscaping/"
+            target="_blank"
+            rel="noreferrer"
+            className="w-14 h-14 rounded-full bg-gradient-to-tr from-yellow-400 via-pink-500 to-purple-600 flex items-center justify-center"
+            aria-label="Instagram"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="w-7 h-7 fill-white"
+            >
+              <path d="M7.75 2h8.5C19.99 2 22 4.01 22 6.25v8.5C22 17.99 19.99 20 16.25 20h-8.5C4.01 20 2 17.99 2 15.75v-8.5C2 4.01 4.01 2 7.75 2zm0 1.5C4.84 3.5 3.5 4.84 3.5 7.75v8.5c0 2.91 1.34 4.25 4.25 4.25h8.5c2.91 0 4.25-1.34 4.25-4.25v-8.5c0-2.91-1.34-4.25-4.25-4.25h-8.5zM12 7a5 5 0 110 10 5 5 0 010-10zm0 1.5a3.5 3.5 0 100 7 3.5 3.5 0 000-7zm5.25-.88a1.13 1.13 0 110 2.26 1.13 1.13 0 010-2.26z"/>
             </svg>
           </a>
         </div>
